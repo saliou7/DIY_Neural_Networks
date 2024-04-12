c6f479fd0a187b6c2a18353959baf533ba12cebc
Saliou Barry <salioubary.85@gmail.com>
2024-04-12 16:38:37 +0200

    ajout test multi_classe

--- Class/loss.py
+++ Class/loss.py
@@ -1,29 +1,87 @@
 import numpy as np
 from module import Loss
 
+
 class MSELoss(Loss):
 
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, y:np.array, yhat:np.array):
+    def forward(self, y: np.array, yhat: np.array):
         """
-            :param y: np.array shape : (batch_size , d)
-            :param yhat: np.array shape : (batch_size , d)
-            :return: np.array shape : (batch_size ,)
+        :param y: np.array shape : (batch_size , d)
+        :param yhat: np.array shape : (batch_size , d)
+        :return: np.array shape : (batch_size ,)
         """
-        assert y.shape == yhat.shape, ValueError(f"y et yhat doivent avoir la meme dimension.")
+        assert y.shape == yhat.shape, ValueError(
+            f"y et yhat doivent avoir la meme dimension."
+        )
 
         return np.sum((y - yhat) ** 2, axis=1)
 
     def backward(self, y, yhat):
         """
-            :param y: batch x d
-            :param yhat: batch x d
-            :return: batch x d
+        :param y: batch x d
+        :param yhat: batch x d
+        :return: batch x d
         """
 
-        assert y.shape == yhat.shape, ValueError(f"y et yhat doivent avoir la meme dimension.")
+        assert y.shape == yhat.shape, ValueError(
+            f"y et yhat doivent avoir la meme dimension."
+        )
 
         return 2 * (yhat - y)
-    
+
+
+class CrossEntropyLoss(Loss):
+    def forward(self, y: np.array, yhat: np.array):
+        """
+        :param y: np.array shape : (batch_size , 1) (entiers entre 0 et d-1)
+        :param yhat: np.array shape : (batch_size , d) (nombres entre 0 et 1)
+        :return: np.array shape : (batch_size ,)
+        """
+        y = y.flatten()
+
+        return -np.log(yhat[np.arange(y.size), y])
+
+    def backward(self, y: np.array, yhat: np.array):
+        """
+
+        :param y: np.array shape : (batch_size , 1) (entiers entre 0 et d-1)
+        :param yhat: np.array shape : (batch_size , d) (nombres entre 0 et 1)
+        :return: np.array shape : (batch_size , d)
+        """
+        y = y.flatten()
+        onehot = np.zeros_like(yhat)
+        onehot[np.arange(y.size), y] = 1
+
+        return -onehot / (yhat + 1e-12)
+
+
+class LogSoftMaxCrossEntropy(Loss):
+    def forward(self, y: np.array, yhat: np.array):
+        """
+        :param y: np.array shape : (batch_size , 1) (entiers entre 0 et d-1)
+        :param yhat: np.array shape : (batch_size , d) (nombres entre 0 et 1)
+        :return: np.array shape : (batch_size ,)
+        """
+        y = y.flatten()
+        yhat_max = np.max(yhat, axis=1, keepdims=True)
+        return (
+            -yhat[np.arange(y.size), y]
+            + np.log(np.sum(np.exp(yhat - yhat_max), axis=1))
+            + yhat_max.flatten()
+        )
+
+    def backward(self, y: np.array, yhat: np.array):
+        """
+        :param y: np.array shape : (batch_size , 1) (entiers entre 0 et d-1)
+        :param yhat: np.array shape : (batch_size , d) (nombres entre 0 et 1)
+        :return: np.array shape : (batch_size , d)
+        """
+        y = y.flatten()
+        onehot = np.zeros_like(yhat)
+        onehot[np.arange(y.size), y] = 1
+        yhat_max = np.max(yhat, axis=1, keepdims=True)
+        expy = np.exp(yhat - yhat_max)
+        return yhat - onehot + expy / np.sum(expy, axis=1).reshape(-1, 1)
--- Class/module.py
+++ Class/module.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Loss(ABC):
     @abstractmethod
     def forward(self, y, yhat):
@@ -8,6 +9,7 @@ class Loss(ABC):
     @abstractmethod
     def backward(self, y, yhat):
         pass
+
 
 class Module(ABC):
     def __init__(self):
@@ -26,7 +28,7 @@ class Module(ABC):
 
     def update_parameters(self, gradient_step=1e-3):
         ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
-        self._parameters -= gradient_step*self._gradient
+        self._parameters -= gradient_step * self._gradient
 
     @abstractmethod
     def backward_update_gradient(self, input, delta):
@@ -37,7 +39,7 @@ class Module(ABC):
     def backward_delta(self, input, delta):
         ## Calcul la derivee de l'erreur
         pass
-    
+
     def predict(self, X):
         ## Calcule la prediction
         pass
