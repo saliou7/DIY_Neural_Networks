7627b4fb20aef58304125850420a855f4e89507b
ji-jiao <79673283+ZHANGzhile@users.noreply.github.com>
2024-04-15 03:15:31 +0200

    Add AutoEncoder

---
+++ Class/AutoEncoder.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from module import Module
+from tqdm import tqdm
+from encapsulage import Sequential
+from lineaire import Linear
+from transformations import TanH, Sigmoid
+
+class AutoEncoder(Module):
+    def __init__(self, DIM_IN, fc_act):
+        super().__init__()
+        self.encoder = Sequential(
+            Linear(DIM_IN, 500),
+            fc_act,
+            Linear(500, 100),
+            fc_act,
+            Linear(100, 10),
+            fc_act
+        )
+        self.decoder = Sequential(
+            Linear(10, 100),
+            fc_act,
+            Linear(100, 500),
+            fc_act,
+            Linear(500, DIM_IN),
+            Sigmoid()
+        )
+    def forward(self, x):
+        encoded = x
+        for layer in self.encoder.modules:
+            encoded = layer.forward(encoded)
+
+        decoded = encoded
+        for layer in self.decoder.modules:
+            decoded = layer.forward(decoded)
+
+        return decoded
+    
+    def backward_delta(self, x, y):
+        pass
+
+    def backward_update_gradient(self, x, y):
+        pass
+
+    def zero_grad(self):
+        pass
